f8c2415b55c19d313bbbc9f4e5623ae276d60218
Alireza <aramez@illinois.edu>
2015-09-06 20:48:23 -0500

    forgot to add daq_types.h!! it is added with this commit

--- daq_stm32F4xx/daq_types.h
+++ daq_stm32F4xx/daq_types.h
@@ -3,10 +3,10 @@
  *
  * Code generated for Simulink model :daq.
  *
- * Model version      : 1.48
+ * Model version      : 1.65
  * Simulink Coder version    : 8.6 (R2014a) 27-Dec-2013
  * TLC version       : 8.6 (Jan 30 2014)
- * C/C++ source code generated on  : Sat Aug 29 20:32:14 2015
+ * C/C++ source code generated on  : Thu Sep 03 12:59:16 2015
  *
  * Target selection: stm32F4xx.tlc
  * Embedded hardware selection: STMicroelectronics->STM32F4xx 32-bit Cortex-M4
